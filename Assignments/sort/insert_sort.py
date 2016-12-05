78dc3f1c08877be32e8d36eb5be673f792e6beff
cleland <clelandgt@gmail.com>
2016-12-05 09:03:46 +0800

    update insert sort

---
+++ Assignments/sort/insert_sort.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+def insert_sort(l):
+    for i in range(1, len(l)):
+        curr = l[i]
+        j = i - 1
+        while j >=0 and l[j] > curr:
+            l[j + 1] = l[j]
+            j -= 1
+        l[j + 1] = curr
+    return l
+
+
+def main():
+    l = [5, 2, 4, 6, 1, 3]
+    print 'before sort: ', l
+    insert_sort(l)
+    print 'after sort: ', l
+
+
+if __name__ == '__main__':
+    main()
