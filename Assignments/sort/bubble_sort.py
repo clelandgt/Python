d3e53b183a4537c1555ae11cbd00267805c8cdce
cleland <clelandgt@163.com>
2017-02-28 01:41:30 +0800

    update bubble sort implement

--- Assignments/sort/bubble_sort.py
+++ Assignments/sort/bubble_sort.py
@@ -1,16 +1,26 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+__author__ = 'cleland'
+
+
 def bubble_sort(l):
-    for i in range(len(l)):
-        for j in range(0, len(l)-i-1):
-            if l[j] > l[j+1]:
-                l[j], l[j+1] = l[j+1], l[j]
+    """ 冒泡排序
+    序列顺序从左到右是从小到大，伪代码：
+    for i in [0, n)
+        for j in [n-1, i) #逆序 
+            if a[j] > a[j-1]
+                swap(a[j], a[j-1])
+    """
+    for i in xrange(len(l)):
+        for j in range(i+1, len(l))[::-1]:
+            if l[j] < l[j-1]:
+                l[j], l[j-1] = l[j-1], l[j]
 
 
 def main():
-    l = [5, 2, 4, 6, 1, 3]
-    print 'before sort: ', l
-    bubble_sort(l)
-    print 'after sort: ', l
+    l1 = [5, 2, 4, 6, 1, 3, 1]
+    print 'before sort: ', l1
+    bubble_sort(l1)
+    print 'after sort: ', l1
 
 
 if __name__ == '__main__':
