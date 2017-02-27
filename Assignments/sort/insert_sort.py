38a26cd437dcb5ad98ecd14f1821d32499d887af
cleland <clelandgt@163.com>
2017-02-28 01:15:37 +0800

    update insert sort implement

--- Assignments/sort/insert_sort.py
+++ Assignments/sort/insert_sort.py
@@ -1,22 +1,27 @@
 # -*- coding:utf-8 -*-
+__author__ = 'cleland'
 
 
 def insert_sort(l):
-    for i in range(1, len(l)):
-        curr = l[i]
-        j = i - 1
-        while j >= 0 and l[j] > curr:
-            l[j + 1] = l[j]
-            j -= 1
-        l[j + 1] = curr
-    return l
+    """ 插入排序
+    从左到右是从小到大
+    伪代码：
+        for i in [1, n)
+            for j in [0, i):
+                if a[i] <= a[j]:
+                    swap(a[i], a[j])
+    """
+    for i in xrange(1, len(l)):
+        for j in xrange(0, i):
+            if l[i] <= l[j]:
+                l[i], l[j] = l[j], l[i]
 
 
 def main():
-    l = [5, 2, 4, 6, 1, 3]
-    print 'before sort: ', l
-    insert_sort(l)
-    print 'after sort: ', l
+    l1 = [5, 2, 4, 6, 1, 3]
+    print 'before sort: ', l1
+    insert_sort(l1)
+    print 'after sort: ', l1
 
 
 if __name__ == '__main__':
