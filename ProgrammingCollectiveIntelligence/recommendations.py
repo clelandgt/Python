c78817876249b7cf26c22cb87d517a6797c2a97a
cleland <clelandgt@163.com>
2019-05-26 12:56:25 +0800

    update

--- ProgrammingCollectiveIntelligence/recommendations.py
+++ ProgrammingCollectiveIntelligence/recommendations.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from math import sqrt
 
 
 # set of movies
@@ -53,3 +54,27 @@ critics = {
 		'Superman Returns': 4.0
 	}
 }
+
+
+def sim_distance(prefs, person1, person2):
+	si = {}
+	for item in prefs[person1]:
+		if item in prefs[person2]:
+			si[item]=1
+
+	if len(si) == 0:
+		return 0
+
+	return 1 / (1+sum([pow(prefs[person1][item]-prefs[person2][item], 2) for item in prefs[person1] if item in prefs[person2]]))
+
+
+def main():
+
+	keys = critics.keys()
+	for person1 in keys:
+		for person2 in keys:
+			print 'person1={0} person2={1}  score={2}'.format(person1, person2, sim_distance(critics, person1, person2))
+
+
+if __name__ == '__main__':
+	main()
