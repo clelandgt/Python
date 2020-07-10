1acd85023db3c4a905756a48bcedabdea70d9ac5
cleland <clelandgt@163.com>
2020-07-10 23:16:47 +0800

    ADD: observer model

---
+++ DesignPatterns/ObserverModel/observer_model.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : observer_model.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-07-10
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
