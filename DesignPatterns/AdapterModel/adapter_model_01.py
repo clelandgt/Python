7ac1362a4e6c754c0f4faedbdf8658233703940d
cleland <clelandgt@163.com>
2020-07-13 22:30:01 +0800

    ADD: adapter_model_01

---
+++ DesignPatterns/AdapterModel/adapter_model_01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : adapter_model_01.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-07-13
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
