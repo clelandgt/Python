f7a504fa07d6db75dfb201fcdba7fcf96de8fcd5
cleland <clelandgt@163.com>
2020-08-02 23:52:01 +0800

    ADD: accept_delivery

---
+++ DesignPatterns/AgentPattern/accept_delivery.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @File  : accept_delivery.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-08-02
+# @Desc  :
+
+
+class ReceiveParcel:
+    pass
+
+
+class RealReceive(ReceiveParcel):
+    pass
+
+
+class ProxyReceive(RealReceive):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
