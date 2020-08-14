2a7f96f3cfcc4edb15a21d2e4b5b3a457e0a49e7
cleland <clelandgt@163.com>
2020-08-14 22:51:17 +0800

    UPD: numeral system

---
+++ DesignPatterns/IteratorPattern/numeral_system.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File  : numeral_system.py
+# @Author: clelandgt@163.com
+# @Date  : 2020-08-14
+# @Desc  :
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
