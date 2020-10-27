ab48b7eb6ad8df2f5f7ed39546828312bc18639d
cleland <clelandgt@163.com>
2020-10-27 23:56:36 +0800

    UPD: toy_build

--- DesignPatterns/BuildPattern/toy_build.py
+++ DesignPatterns/BuildPattern/toy_build.py
@@ -27,3 +27,30 @@ class Car(Toy):
         print('我是' + self._name, ', 我可以快速奔跑...')
 
 
+class Manor(Toy):
+    """庄园"""
+    def feature(self):
+        print('我是' + self._name, ', 我可供观赏，也可用来游玩!...')
+
+
+class ToyBuild:
+    def build_car(self):
+        car = Car('迷你小车')
+        print('正在构建' + car.get_name() + '...')
+        car.add_component('轮子', 4)
+        car.add_component('车身')
+        car.add_component('发动机')
+        car.add_component('方向盘')
+        return car
+
+    def build_manor(self):
+        manor = Manor('淘淘小庄园')
+        print('正在构建' + manor.get_name() + '...')
+        manor.add_component('客厅', 1, '间')
+        manor.add_component('卧室', 2, '间')
+        manor.add_component('卫生间', 1, '间')
+        manor.add_component('厨房', 1, '间')
+        manor.add_component('阳台', 1, '间')
+        return manor
+
+
